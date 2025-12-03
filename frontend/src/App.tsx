ae4dc41a91e318a5378e0da60ec51f6d279aad60
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-12-03 17:04:56 +0100

    index.css add padding to body and remove place-items: center

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,28 +12,31 @@ function App() {
 
     useEffect(() => {
 		const fetchMessage = async () => {
-		try {
-		const response = await fetch(`${API_URL}/api/message`);
-		if (!response.ok) {
-		throw new Error(`HTTP error ${response.status}`);
-		}
-		const data = (await response.json()) as ApiResponse;
-		setMessage(data.message);
-		setError(null);
-		} catch {
-		setError("Erreur lors de l'appel API");
-		}
+			try {
+				const response = await fetch(`${API_URL}/api/message`);
+
+				if (!response.ok) {
+					throw new Error(`HTTP error ${response.status}`);
+				}
+
+				const data = (await response.json()) as ApiResponse;
+				setMessage(data.message);
+				setError(null);
+			}
+			catch {
+				setError("Erreur lors de l'appel API");
+			}
 		};
 
         fetchMessage();
     }, []);
 
     return (
-        <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+        <>
             <h1>Mini projet fullstack</h1>
             <p>Message du backend :</p>
             {error ? <pre>{error}</pre> : <pre>{message}</pre>}
-        </div>
+        </>
     );
 }
 
